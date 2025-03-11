bbdbebaa203d29bfa6270b10e5bc21391c1e3f29
fares-ma <168366189+fares-ma@users.noreply.github.com>
2025-03-12 01:25:13 +0200

    Implementation Of DAL

---
+++ Company.Fares.DAL/Data/Contexts/CompanyDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Company.Fares.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Fares.DAL.Data.Contexts
+{
+    internal class CompanyDbContext : DbContext
+    {
+
+        public CompanyDbContext() : base()
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+
+            optionsBuilder.UseSqlServer("Server=.;Database=CompanyDb;Trusted_Connection=True;TrustServerCertificate = True");
+        }
+
+        public DbSet<Department>  departments { get; set; }
+    }
+}
