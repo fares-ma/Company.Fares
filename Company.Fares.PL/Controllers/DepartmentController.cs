0c91bde16643eff331a54ae724658c34ea04134b
fares-ma <168366189+fares-ma@users.noreply.github.com>
2025-03-14 14:20:29 +0200

    Create Create (Action-View)

--- Company.Fares.PL/Controllers/DepartmentController.cs
+++ Company.Fares.PL/Controllers/DepartmentController.cs
@@ -1,6 +1,7 @@
 ﻿using Company.Fares.BLL.Interfaces;
 using Company.Fares.BLL.Repositories;
 using Company.Fares.DAL.Models;
+using Company.Fares.PL.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.Fares.PL.Controllers
@@ -23,5 +24,38 @@ namespace Company.Fares.PL.Controllers
 
             return View(departments);
         }
+
+        [HttpGet]
+        public IActionResult Create() 
+        { 
+       
+            return View();
+        
+        }
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if (ModelState.IsValid) // Server Side Validation
+            {
+                var department = new Department() 
+                {
+                    Code = model.Code,
+                    Name = model.Name,
+                    CreateAt = model.CreateAt
+                };
+              var count =  _departmentRepository.Add(department);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+            }
+
+            return View();
+
+        }
+
+
+
     }
 }
