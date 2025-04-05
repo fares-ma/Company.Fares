3c65b033ec34cde65cb291596a1a43ce7af4bf39
fares-ma <168366189+fares-ma@users.noreply.github.com>
2025-04-06 00:14:13 +0200

    Save Changes In Session 8

---
+++ Company.Fares.PL/Dtos/UserInRoleDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Company.Fares.PL.Dtos
+{
+    public class UserInRoleDto
+    {
+        public string UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public bool IsSelected { get; set; }
+    }
+}
